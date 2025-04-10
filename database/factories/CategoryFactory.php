91f33c359dbf3be4396fd653d95b008531546ec8
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2025-04-10 16:38:45 -0400

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,76 +1,18 @@
 <?php
 
-namespace App\Models;
+namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-class TransactionItem extends Model
+class CategoryFactory extends Factory
 {
-    use HasFactory;
+    protected $model = \App\Models\Category::class;
 
-    protected $fillable = [
-        'transaction_id',
-        'category_id',
-        'description',
-        'amount',
-    ];
-
-    public function transaction()
+    public function definition()
     {
-        return $this->belongsTo(Transaction::class);
-    }
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
+        return [
+            'name' => $this->faker->word,
+        ];
     }
 }
-
-// class Transaction extends Model
-// {
-//     use HasFactory;
-
-//     protected $fillable = [
-//         'user_id',
-//         'user_group_id',
-//         'description',
-//         'total',
-//         'is_income',
-//         'recurring',
-//         'transaction_schedule_id',
-//         'income_source_id',
-//         'transaction_date',
-//     ];
-
-//     protected $casts = [
-//         'transaction_date' => 'datetime',
-//         'is_income' => 'boolean',
-//         'recurring' => 'boolean',
-//     ];
-
-//     public function user()
-//     {
-//         return $this->belongsTo(User::class);
-//     }
-
-//     public function userGroup()
-//     {
-//         return $this->belongsTo(UserGroup::class);
-//     }
-
-//     public function transactionSchedule()
-//     {
-//         return $this->belongsTo(TransactionSchedule::class);
-//     }
-
-//     public function incomeSource()
-//     {
-//         return $this->belongsTo(IncomeSource::class);
-//     }
-
-//     public function transactionItems()
-//     {
-//         return $this->hasMany(TransactionItem::class);
-//     }
-// }
